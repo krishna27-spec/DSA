a6f1f2471e9fd40c376a57b2ae46fcab4f68c1cd
ksrikrisham2020@gmail.com <your_email@example.com>
2026-06-10 17:37:59 +0530

    algo

--- Sorting and order statistics/Quick_sort/quick_sort.cpp
+++ Sorting and order statistics/Quick_sort/quick_sort.cpp
@@ -2,5 +2,40 @@
 #include <vector>
 using namespace std;
 
-int 
+int partition(vector<int>&A, int p, int r){
+    int x = A[r];
+    int i = p-1;
+    for(int j =0; j<r-1; j++){
+        if(A[j]<x){
+             i++;
+             swap(A[i],A[j]);
+        }
+    }
+    swap(A[i+1],A[r]]);
+    return i+1;
+
+}
+
+void quicksort(vector<int>&A, int p, int r){
+    if(p<r){
+        int q = partition(A,p,r);
+        quicksort(A, p, q-1);
+        quicksort(A, q+1,p);
+    }
+}
+
+int main(){
+    int n; cin >> n;
+    vector<int> num1;
+    for(int i =0; i<n; i++){
+        int num; cin>>num;
+        num1.push_back(num);
+    }
+    quicksort(num1,0,num1.size()-1);
+    for(int i =0; i<n;i++){
+        cout << num1[i] << " ";
+    }
+    cout << endl;
+
+}
 
